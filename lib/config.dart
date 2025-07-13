95e0916d653faeffd3594c5e3b03d8ba6f3bb647
sodicbkp <sodic.bkp@gmail.com>
2025-07-13 13:47:43 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,25 +1,25 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppConfig {
-  static const String _defaultBaseUrl = "https://937279d75870.ngrok-free.app";
-  static const String _key = "baseUrl";
+  static String _baseUrl = "https://937279d75870.ngrok-free.app/admin";
+
+  static Future<void> setBaseUrl(String url) async {
+    _baseUrl = url;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('base_url', url);
+  }
 
   static Future<String> getBaseUrl() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_key) ?? _defaultBaseUrl;
+    return prefs.getString('base_url') ?? _baseUrl;
   }
 
-  static Future<void> setBaseUrl(String url) async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(_key, url);
-  }
-
-  static Future<String> getSupportUsersUrl() async =>
+  static Future<String> get supportUsersUrl async =>
       "${await getBaseUrl()}/support-users";
 
-  static Future<String> getSupportMessagesUrl(String userId) async =>
+  static Future<String> supportMessagesUrl(String userId) async =>
       "${await getBaseUrl()}/support-messages?user_id=$userId";
 
-  static Future<String> getAdminReplyUrl() async =>
+  static Future<String> get adminReplyUrl async =>
       "${await getBaseUrl()}/admin-reply";
 }
